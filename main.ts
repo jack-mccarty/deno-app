1bc15849416cb0d68120b4247bc677dc96a62b27
jackm <jack.mccarty@outlook.com>
2023-04-08 22:16:49 -0500

    testing basic html

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serveDir, serveFile } from "https://deno.land/std@0.182.0/http/file_server.ts";
 
-serve((req: Request) => new Response("Hello cheese"));
+import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
+
+serve((req: Request) => {
+
+  const pathname = new URL(req.url).pathname;
+
+  if (pathname === "/simple_file") {
+
+    return serveFile(req, "./index.html");
+  }
+
+  if (pathname.startsWith("/static")) {
+
+    return serveDir(req, {
+      fsRoot: "public",
+      urlRoot: "static",
+    });
+  }
+
+  return new Response("404: Not Found", {
+    status: 404,
+  });
+});
