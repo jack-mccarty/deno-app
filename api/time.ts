37a0d9c7a4c640c6a12aca30c0d4123e97820114
jackm <jack.mccarty@outlook.com>
2023-04-08 23:53:25 -0500

    basic outline with api routes. More to come

---
+++ api/time.ts
@@ -0,0 +1,45 @@
+//function that creates the date time object
+// {day: 1, weekday: saturday, month: 1, year: 2020, hour, minute, second, ampm}
+//
+
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+export function getDateTime() {
+  const now = new Date();
+  return JSON.stringify({
+    day: now.getDate(),
+    weekday: weekdays[now.getDay()],
+    month: months[now.getMonth()],
+    year: now.getFullYear(),
+    hour: now.getHours() === 0
+      ? 12
+      : now.getHours() > 12
+      ? now.getHours() - 12
+      : now.getHours(),
+    minute: now.getMinutes(),
+    second: now.getSeconds(),
+    //determine if am or pm
+    ampm: now.getHours() >= 12 ? "pm" : "am",
+  });
+}
--- main.ts
+++ main.ts
@@ -1,22 +1,46 @@
-import { serveDir, serveFile } from "https://deno.land/std@0.182.0/http/file_server.ts";
+import {
+  serveDir,
+  serveFile,
+} from "https://deno.land/std@0.182.0/http/file_server.ts";
 
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
-serve((req: Request) => {
+import * as time from "./api/time.ts";
 
+const text200 =  {
+  status: 200,
+  headers: {
+    "Content-Type": "text/plain",
+  },
+};
+
+const json200 =  {
+  status: 200,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+serve((req: Request) => {
   const pathname = new URL(req.url).pathname;
 
-  if (pathname === "/simple_file") {
-
-    return serveFile(req, "./index.html");
+  if (pathname === "/") {
+    return serveFile(req, "./public/index.html");
   }
 
-  if (pathname.startsWith("/static")) {
-
+  if (pathname.startsWith("/public")) {
     return serveDir(req, {
       fsRoot: "public",
-      urlRoot: "static",
+      urlRoot: "public",
     });
+  }
+
+  if (pathname.startsWith("/api")) {
+    //get the path after /api
+    const apiPath = pathname.replace("/api", "");
+    if (apiPath === "/time") {
+      return new Response(time.getDateTime(), json200);
+    }
   }
 
   return new Response("404: Not Found", {
