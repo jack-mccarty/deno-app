5077557860db28ca8283809fe7d522fa67b9452c
Jack McCarty <51275481+jack-mccarty@users.noreply.github.com>
2023-04-10 11:50:34 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,13 +1,1 @@
-const headHTML = `
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/styles/pico.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
-
-    <link rel="stylesheet" href="/public/styles/main.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/base16/material.min.css" integrity="sha512-FKzMeNkm8zqCguwqHyTYskFTD4L7WW5znImGuc+fYTIJGRpUWszuJLGh9Bq8smPaPzN0LtqagnRgihN53PL04A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-`;
-
-//insert the headHTML into the head of the document
-document.head.innerHTML += headHTML;
+//nothing here yet
